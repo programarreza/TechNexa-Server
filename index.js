9fc308f942544c550293fd6f0a1e7e8cb1e85432
programmarreza <programarreza@gmail.com>
2023-10-20 19:14:46 +0600

    details product show get single data

--- index.js
+++ index.js
@@ -93,19 +93,20 @@ async function run() {
 
     })
 
-    // // single data
-    // app.get('/products/:id', async(req, res) =>{
-    //   const id = req.params.id;
-    //   const query = {_id: new ObjectId(id)}
-    //   const result = await productCollections.findOne(query)
-    //   res.send(result)
-    // })
+    // details
+    app.get('/product-details/:id', async(req, res) =>{
+      const id = req.params.id;
+      console.log(id);
+      const query = {_id: new ObjectId(id)}
+      const result = await productCollections.findOne(query)
+      res.send(result)
+    })
 
 
     // update read
     app.get('/product-update/:id', async (req, res) => {
       const id = req.params.id;
-      console.log(108, id);
+      // console.log(108, id);
       const query = { _id: new ObjectId(id) }
       const result = await productCollections.findOne(query)
       res.send(result)
